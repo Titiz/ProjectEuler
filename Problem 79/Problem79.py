a9790b02d218e66b30c9c6624c4b56fbcd1f2dcb
titiz <gtitiz@gmail.com>
2017-07-01 12:23:53 +0300

    Correct Answer Gotten

--- Problem 79/Problem79.py
+++ Problem 79/Problem79.py
@@ -1,28 +1,18 @@
 f = open("codes.txt", "r")
 
-
-class integer:
-    value = 0
-    left = []
-    right = []
-
-
-
-
-order = []
 neighbors = []
 
 for i in range(10):
-    print(i)
     neighbors.append([[], []])
 
 
-
+used_numbers = []
 
 for line in f:
     for i in range(3):
         integer = int(line[i])
-        print("CURRENT INTEGER", integer)
+        if integer not in used_numbers:
+            used_numbers.append(integer)
         if i == 0:
             neighbors[integer][1].append(int(line[1]))
             neighbors[integer][1].append(int(line[2]))
@@ -31,17 +21,23 @@ for line in f:
             neighbors[integer][1].append(int(line[2]))
         elif i == 2:
             neighbors[integer][0].append(int(line[1]))
-            neighbors[integer][1].append(int(line[0]))
+            neighbors[integer][0].append(int(line[0]))
         
         neighbors[integer][0] = list(set(neighbors[integer][0]))
         neighbors[integer][1] = list(set(neighbors[integer][1]))
 
-for i in range(10):
-    print(i, "  :", len(neighbors[i][1]))
-    print(i, " LEFT: ", neighbors[i][0])
-    print(i, "RIGHT: ", neighbors[i][1])
+answer_list = [None] * len(used_numbers)
 
-        
+for i in used_numbers:
+    answer_list[len(neighbors[i][0])] = i
+
+string = ""
+for item in answer_list:
+    string += str(item)
+
+print(string)
+
+f.close()
 
 
 
